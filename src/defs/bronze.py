583df3b9c8ee7c49b0a8353e22beb730d2bad7f0
Phong Thanh <thanhphong1110n@gmail.com>
2026-03-16 14:14:44 +0700

    Create bronze.py

---
+++ src/defs/bronze.py
@@ -0,0 +1,29 @@
+import dagster as dg
+from dotenv import load_dotenv
+import requests
+import os
+from  pathlib import Path
+import json
+import datetime
+load_dotenv()
+
+@dg.asset(group_name="bronze")
+def fetch_data_from_API():
+    """
+        Extract data from API.Source from Nasa
+    """
+    url = "https://api.nasa.gov/neo/rest/v1/feed"
+    params= {
+        "start_date":((datetime.date.today() - datetime.timedelta(days=7))).isoformat(),
+        "end_date":datetime.date.today().isoformat(),
+        "api_key":os.getenv("API_KEY")
+    }
+    resp = requests.get(url,params=params)
+    path = Path("src/nasa_project/defs/data/nasa_data.json")
+    with open(path,"w") as f:
+        json.dump(resp.json(),f,indent=2)
+    return resp.json()
+
+    
+
+    
