26322ddd174cf29fb6392818c80d3289dec5f10c
Phong Thanh <thanhphong1110n@gmail.com>
2026-03-16 14:19:11 +0700

    Create transform_data_gg_form.py

---
+++ src/defs/transform_data_gg_form.py
@@ -0,0 +1,16 @@
+import dagster as dg
+import json
+@dg.asset(
+    deps =["ingest_data_from_csv"],
+    group_name="email"
+)
+def transform_gg_form_data(ingest_data_from_csv):
+    """
+    tranfrom data from gg form
+    """
+    with open(ingest_data_from_csv) as f:
+        data = json.load(f)
+    email=list(data["Your email"].values())
+    path = "src/nasa_project/defs/data/email.json"
+    with open(path,"w") as f:
+        json.dump(email,f,indent=2)
